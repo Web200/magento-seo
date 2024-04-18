d5bf81fb983cbdb70fef055e6175ae783d90a545
unknown <ali.hammam@scandiweb.com>
2024-04-19 01:01:47 +0200

    Seo: Adding graphql query for metarobots.

---
+++ Api/CmsIndexMetaRobotsInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Web200\Seo\Api;
+
+/**
+ * Interface CmsIndexMetaRobotsInterface
+ *
+ * @package   Web200\Seo\Api
+ * @author    Web200 <contact@web200.fr>
+ * @copyright 2021 Web200
+ * @license   https://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://www.web200.fr/ */
+interface CmsIndexMetaRobotsInterface extends MetaRobotsInterface
+{
+}
